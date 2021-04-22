825efe96113e32a48bdc858b95e202074dcaf0bd
rheadash <rhea.dash@gmail.com>
2021-04-22 13:36:19 -0700

    Added moving stones from pit to pit.

--- sandstones/src/DrawBoard.java
+++ sandstones/src/DrawBoard.java
@@ -2,13 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.EventListener;
 import java.util.Random;
 
 
 public class DrawBoard {
+    static BoardPainter board;
 
     /**
      * paints a full board
@@ -16,6 +19,7 @@ public class DrawBoard {
     public static class BoardPainter extends JPanel {
         private ArrayList<Pit> pots = new ArrayList<>();
         private java.util.List<Integer> mancalaBoard;
+        private ArrayList<EventListener> listeners = new ArrayList<>();
         private Style style;
 
         public BoardPainter(Style style, java.util.List<Integer> mancalaBoard) {
@@ -26,37 +30,95 @@ public class DrawBoard {
             // a 2x6 grid in the center
             JPanel center = new JPanel();
             center.setLayout(new GridLayout(2, 6));
+
             for (int i = 0; i < 12; i++) {
                 Pit p = new Pit(style, i);
                 pots.add(p);
+                listeners.add(p.getListener());
                 center.add(p);
             }
+
+
             add(center, BorderLayout.CENTER);
 
             // one side
-            Pit east = new Pit(style, 13);
+            Pit east = new Pit(style, 12);
+            listeners.add(east.getListener());
             pots.add(east);
             add(east, BorderLayout.EAST);
             east.setPreferredSize(new Dimension(100, 0));
 
             // the other side
-            Pit west = new Pit(style, 14);
+            Pit west = new Pit(style, 13);
+            listeners.add(west.getListener());
             pots.add(west);
             add(west, BorderLayout.WEST);
             west.setPreferredSize(new Dimension(100, 0));
+
+            // link pits to each other for dropping stones
+            for (int i = 1; i < 5; i++) {
+                pots.get(i).setNext(pots.get(i - 1));
+                pots.get(i).setPrev(pots.get(i + 1));
+            }
+            pots.get(0).setPrev(pots.get(1));
+            pots.get(0).setNext(pots.get(13));
+
+            pots.get(5).setPrev(pots.get(12));
+            pots.get(5).setNext(pots.get(4));
+
+            for (int i = 7; i < 11; i++) {
+                pots.get(i).setNext(pots.get(i + 1));
+                pots.get(i).setPrev(pots.get(i - 1));
+            }
+            pots.get(6).setPrev(pots.get(13));
+            pots.get(6).setNext(pots.get(7));
+
+            pots.get(11).setPrev(pots.get(10));
+            pots.get(11).setNext(pots.get(12));
+
+            pots.get(12).setPrev(pots.get(11));
+            pots.get(12).setNext(pots.get(5));
+
+            pots.get(13).setPrev(pots.get(0));
+            pots.get(13).setNext(pots.get(6));
+
+            for (Pit p : pots) {
+                System.out.println(p);
+            }
+
+            for (int i = 0; i < mancalaBoard.size(); i++) {
+                pots.get(i).setStones(mancalaBoard.get(i));
+            }
         }
 
         // This method will handle moving stones when a pit is clicked.
         public void pitPressed(int position) {
             System.out.println("Pit " + position + " has been clicked.");
-            pots.get(position);
+            if (position == 12 || position == 13)
+                return; // TODO add error message saying goal pits can't be clicked
+            // TODO this should NOT count as a move or cause game to switch to other player
+            Pit current = pots.get(position);
+            int stones = current.getStones();
+            if (stones == 0)
+                return; // TODO add error message saying empty pits don't count as a move
+            // TODO this should NOT count as a move or cause game to switch to other player
+
+            // TODO check whether this pit belongs to the current player, add error message if not
+
+            Pit next = current.getNext();
+            while (stones > 0) {
+                current.loseStone();
+                next.addStone();
+                this.repaint();
+                next = next.getNext();
+                stones--;
+            }
+            System.out.println("After stones loop");
+
         }
 
-        public void paint(Graphics g) {
 
-            for (int i = 0; i < mancalaBoard.size(); i++) {
-                pots.get(i).setStones(mancalaBoard.get(i));
-            }
+        public void paint(Graphics g) {
             super.paint(g);
         }
     }
@@ -68,17 +130,70 @@ public class DrawBoard {
         private static final int MARGIN = 2;
         private static final int STONE_SIZE = 10;
         private int stones = 0;
-        private int position;
+        private final int position;
+        private String label;
+        public Pit next;
+        public Pit prev;
+        private MouseListener listener;
         private Style style;
 
         public Pit(Style style, int pos) {
             this.style = style;
             this.position = pos;
-            addMouseListener(new MousePressedListener());
+            addMouseListener(this.listener = new MousePressedListener());
+        }
+
+        public int getPosition() {
+            return position;
+        }
+
+        public Pit getPrev() {
+            return prev;
+        }
+
+        public Pit getNext() {
+            return next;
+        }
+
+        public void setPrev(Pit p) {
+            this.prev = p;
+        }
+
+        public void setNext(Pit p) {
+            this.next = p;
+        }
+
+        public MouseListener getListener() {
+            return this.listener;
         }
 
         public void setStones(int stones) {
             this.stones = stones;
+        }
+
+        public int getStones() {
+            return this.stones;
+        }
+
+        public void addStone() {
+            stones++;
+        }
+
+        public void loseStone() {
+            stones--;
+        }
+
+        public void setStyle(Style newStyle) {
+            this.style = newStyle;
+        }
+
+        public String toString() {
+            String str = "";
+            str += "\nPit #" + position;
+            str += "\nStones: " + stones;
+            str += "\nNext: " + next.position;
+            str += "\nPrev: " + prev.position;
+            return str;
         }
 
         public void paint(Graphics g) {
@@ -105,21 +220,32 @@ public class DrawBoard {
             @Override
             public void mousePressed(MouseEvent event) {
                 Point mousePoint = event.getPoint();
-                System.out.println("Pit # " + position + " clicked");
                 Ellipse2D.Double pitBoundaries = new Ellipse2D.Double(MARGIN, MARGIN, getWidth() - MARGIN * 2, getHeight() - MARGIN * 2);
-                System.out.println(mousePoint.getX() + " " + mousePoint.getY());
-                System.out.println(pitBoundaries.contains(mousePoint.getX(), mousePoint.getY()));
+                if (pitBoundaries.contains(mousePoint.getX(), mousePoint.getY()))
+                    board.pitPressed(position);
             }
+        }
+    }
+
+    public static class End extends Pit {
+
+        public End(Style style, int pos) {
+            super(style, pos);
         }
     }
 
 
     public static void main(String... args) {
-        Style boardStyle = new BlueStyle();
+        Style boardStyle = new PinkStyle();
+        board = new BoardPainter(boardStyle, new ArrayList<>(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 4, 2)));
         SwingUtilities.invokeLater(() -> {
             JFrame jf = new JFrame();
-            jf.add(new BoardPainter(boardStyle, new ArrayList<>(
-                    Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 4, 2))));
+            /*
+            Add button for changing styles here. Whenever new style is chosen, call redraw method
+             on board.
+             */
+            jf.add(board);
             jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             jf.setSize(800, 300);
             jf.setVisible(true);
